cdd155c62677b735201ccaa05cc1746fe03d1eee
Tommi Berg <tommi.berg@elisa.fi>
2015-08-14 13:58:23 +0300

    token

--- app/helloworld/api/__init__.py
+++ app/helloworld/api/__init__.py
@@ -1,11 +1,33 @@
 from tornado_json.requesthandlers import APIHandler
 
 class TowerAPIHandler(APIHandler):
-    __url_names__ = ["tower"]
+    __url_names__ = ["helloworld"]
 
 class Handler(TowerAPIHandler):
     def get(self):
         try:
             self.success("hello world")
+        except:
+            self.fail("Failed")
+
+class UpdateHandler(TowerAPIHandler):
+    def get(self, token, key, value):
+        try:
+            if (token == self.application.settings['token']):
+                print key + ": " + value + "\n"
+                self.success("Success")
+            else:
+                raise Error("Not supported")
+        except:
+            self.fail("Failed")
+
+class GetHandler(TowerAPIHandler):
+    def get(self, token, key):
+        try:
+            if (token == self.application.settings['token']):
+                print key + "\n"
+                self.success("Success")
+            else:
+                raise Error("Not supported")
         except:
             self.fail("Failed")
